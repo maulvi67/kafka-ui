99bacc2a80ffe734517d7fe9543642fd39884c60
anezboretskiy <anezboretskiy@provectus.com>
2022-09-19 11:38:39 +0300

    Base test

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/base/BaseTest.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/base/BaseTest.java
@@ -37,6 +37,7 @@ public class BaseTest {
 
   public static final String SELENIUM_IMAGE_NAME = "selenium/standalone-chrome:103.0";
   public static final String SELENIARM_STANDALONE_CHROMIUM = "seleniarm/standalone-chromium:103.0";
+  public static final String CLUSTER_NAME = "local";
   protected Pages pages = Pages.INSTANCE;
   protected Helpers helpers = Helpers.INSTANCE;
 
