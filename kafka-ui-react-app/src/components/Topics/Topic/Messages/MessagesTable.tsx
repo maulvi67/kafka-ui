91d744d0d4e0a49e93a98e76f4771034a82c5917
Mgrdich <mgotm13@gmail.com>
2022-11-01 17:28:31 +0400

    initial messages pagination , modification in v2

--- kafka-ui-react-app/src/components/Topics/Topic/Messages/MessagesTable.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Messages/MessagesTable.tsx
@@ -9,54 +9,102 @@ import {
 } from 'redux/reducers/topicMessages/selectors';
 import TopicMessagesContext from 'components/contexts/TopicMessagesContext';
 import { useAppSelector } from 'lib/hooks/redux';
+import { Button } from 'components/common/Button/Button';
+import handleNextPageClick from 'components/Topics/Topic/MessagesV2/utils/handleNextPageClick';
+import { ConsumingMode } from 'lib/hooks/api/topicMessages';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MESSAGES_PER_PAGE } from 'lib/constants';
+import * as S from 'components/common/NewTable/Table.styled';
 
 import Message from './Message';
 
 const MessagesTable: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const { isLive } = useContext(TopicMessagesContext);
+  const navigate = useNavigate();
 
   const messages = useAppSelector(getTopicMessges);
   const isFetching = useAppSelector(getIsTopicMessagesFetching);
 
+  const mode = searchParams.get('m') as ConsumingMode;
+
+  const isTailing = mode === 'live' && isFetching;
+
+  // Pagination is disabled in live mode, also we don't want to show the button
+  // if we are fetching the messages or if we are at the end of the topic
+  const isPaginationDisabled =
+    isTailing || isFetching || !searchParams.get('seekTo');
+
+  const isNextPageButtonDisabled =
+    isPaginationDisabled ||
+    messages.length < Number(searchParams.get('perPage') || MESSAGES_PER_PAGE);
+  const isPrevPageButtonDisabled =
+    isPaginationDisabled || !searchParams.get('page');
+
+  const handleNextPage = () =>
+    handleNextPageClick(messages, searchParams, setSearchParams);
+
   return (
-    <Table isFullwidth>
-      <thead>
-        <tr>
-          <TableHeaderCell> </TableHeaderCell>
-          <TableHeaderCell title="Offset" />
-          <TableHeaderCell title="Partition" />
-          <TableHeaderCell title="Timestamp" />
-          <TableHeaderCell title="Key" />
-          <TableHeaderCell title="Value" />
-          <TableHeaderCell> </TableHeaderCell>
-        </tr>
-      </thead>
-      <tbody>
-        {messages.map((message: TopicMessage) => (
-          <Message
-            key={[
-              message.offset,
-              message.timestamp,
-              message.key,
-              message.partition,
-            ].join('-')}
-            message={message}
-          />
-        ))}
-        {isFetching && isLive && !messages.length && (
+    <>
+      <Table isFullwidth>
+        <thead>
           <tr>
-            <td colSpan={10}>
-              <PageLoader />
-            </td>
+            <TableHeaderCell> </TableHeaderCell>
+            <TableHeaderCell title="Offset" />
+            <TableHeaderCell title="Partition" />
+            <TableHeaderCell title="Timestamp" />
+            <TableHeaderCell title="Key" />
+            <TableHeaderCell title="Value" />
+            <TableHeaderCell> </TableHeaderCell>
           </tr>
-        )}
-        {messages.length === 0 && !isFetching && (
-          <tr>
-            <td colSpan={10}>No messages found</td>
-          </tr>
-        )}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {messages.map((message: TopicMessage) => (
+            <Message
+              key={[
+                message.offset,
+                message.timestamp,
+                message.key,
+                message.partition,
+              ].join('-')}
+              message={message}
+            />
+          ))}
+          {isFetching && isLive && !messages.length && (
+            <tr>
+              <td colSpan={10}>
+                <PageLoader />
+              </td>
+            </tr>
+          )}
+          {messages.length === 0 && !isFetching && (
+            <tr>
+              <td colSpan={10}>No messages found</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      <S.Pagination>
+        <S.Pages>
+          <Button
+            buttonType="secondary"
+            buttonSize="L"
+            disabled={isPrevPageButtonDisabled}
+            onClick={() => navigate(-1)}
+          >
+            ← Back
+          </Button>
+          <Button
+            buttonType="secondary"
+            buttonSize="L"
+            disabled={isNextPageButtonDisabled}
+            onClick={handleNextPage}
+          >
+            Next →
+          </Button>
+        </S.Pages>
+      </S.Pagination>
+    </>
   );
 };
 
--- kafka-ui-react-app/src/components/Topics/Topic/Messages/__test__/MessagesTable.spec.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Messages/__test__/MessagesTable.spec.tsx
@@ -71,9 +71,9 @@ describe('MessagesTable', () => {
   });
 
   describe('Custom Setup with different props value', () => {
-    it('should check if next click is gone during isLive Param', () => {
+    it('should check if next click is disabled isLive Param', () => {
       setUpComponent(searchParams, { ...contextValue, isLive: true });
-      expect(screen.queryByText(/next/i)).not.toBeInTheDocument();
+      expect(screen.queryByText(/next/i)).toBeDisabled();
     });
 
     it('should check the display of the loader element', () => {
